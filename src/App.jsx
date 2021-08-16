335642b05a9bbec6b94faef85135c67ed4615e52
Mehul Mohan <mehulmpt@gmail.com>
2021-08-17 00:06:01 +0530

    lab

--- src/App.jsx
+++ src/App.jsx
@@ -44,19 +44,24 @@ export default function App() {
 	]
 
 	function handleAnswerClick() {
-		// Check if the quiz is over here
+		// Check if correct answer is pressed. (See the hint on the left)
 
-		// If yes, set the quizFinished variable to true
-
-		// If no, increment the current index like always
-		setCurrentIndex((value) => value + 1)
+		if (currentIndex === questions.length - 1) {
+			// quiz over
+			setQuizFinished(true)
+		} else {
+			setCurrentIndex((value) => value + 1)
+		}
 	}
 
 	const [quizFinished, setQuizFinished] = useState(false)
 
+	// Create a state variable here [score, setScore]
+
 	return (
 		<div className="app">
 			{quizFinished ? (
+				/* Change this hardcoded 1 to state variable score else */
 				<div className="score-section">
 					You scored 1 out of {questions.length}
 				</div>
@@ -66,12 +71,10 @@ export default function App() {
 						<div className="question-count">
 							<span>Question 1</span>/{questions.length}
 						</div>
-						{/* You should change the "0" here to a state variable */}
 						<div className="question-text">
 							{questions[currentIndex].questionText}
 						</div>
 					</div>
-					{/* You should change the "0" here to a state variable */}
 					<div className="answer-section">
 						{questions[currentIndex].answerOptions.map((answer) => {
 							// Add onClick listener to this button
